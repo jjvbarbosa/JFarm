472141d83833a2cc4bbb7b3564a56470d84b8bff
jjvbarbosa <joaovitor123war@hotmail.com>
2024-01-26 16:08:00 -0300

    🎉 init: Create project

---
+++ src/main/java/com/jvbarbosa/jfarm/JFarmApplication.java
@@ -0,0 +1,13 @@
+package com.jvbarbosa.jfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JFarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JFarmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jvbarbosa/jfarm/JFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvbarbosa.jfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JFarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
